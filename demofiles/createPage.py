9f82bd325f2b2204cc948d9cd4e10b801a9b1391
garrett-hannah <144636059+Garrett-Hannah@users.noreply.github.com>
2025-02-27 20:56:30 -0500

    More Changes! This is a mess!
    Ok mostly rearranged everything so that it can be a bit more reactive, instead of just static. Other than that i think its mostly alright

--- demofiles/createPage.py
+++ demofiles/createPage.py
@@ -1,25 +1,24 @@
-from flask import Flask, render_template_string
+from flask import Flask, render_template
 import pandas as pd
 
 app = Flask(__name__)
 
 @app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/review')
+def review():
+    return render_template('reviewPage.html')
+
+
+
+@app.route('/database')
 def index():
     df = pd.read_csv('geonames.csv')  # Load CSV
+    df = df.head(19)
     table = df.to_html(classes='table table-striped', index=False)  # Convert to HTML table
-    return render_template_string("""
-        <html>
-        <head>
-            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-        </head>
-        <body>
-            <div class="container">
-                <h2>CSV Data</h2>
-                {{ table | safe }}
-            </div>
-        </body>
-        </html>
-    """, table=table)
+    return render_template('database.html', table=table)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
