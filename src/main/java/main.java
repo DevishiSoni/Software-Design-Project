acdd8a1a5dcc4639f05a19307cdc297e6ee11871
garrett-hannah <144636059+Garrett-Hannah@users.noreply.github.com>
2025-03-21 00:50:03 -0400

    Changes to pom

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,5 +1,8 @@
+import TourCatGUI.HomePage;
+
 public class main {
     public static void main(String[] args) {
         //TODO: setup a system where this stuff can work
+        HomePage homePage = new HomePage("username");
     }
 }
